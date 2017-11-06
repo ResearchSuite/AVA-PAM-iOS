183a5936ec4a998e7ed32ca7ef24193adbd6f38f
christinatsan <ctsango000@citymail.cuny.edu>
2017-11-06 17:48:51 -0500

    Initial Commit

---
+++ PAM-Reference-AppTests/PAM_Reference_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  PAM_Reference_AppTests.swift
+//  PAM-Reference-AppTests
+//
+//  Created by Christina Tsangouri on 11/6/17.
+//  Copyright © 2017 Christina Tsangouri. All rights reserved.
+//
+
+import XCTest
+@testable import PAM_Reference_App
+
+class PAM_Reference_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
